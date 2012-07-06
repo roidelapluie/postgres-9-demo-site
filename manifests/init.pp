b349d36c10f67860e67953c4d80060b0eefbb88d
Julien Pivotto <roidelapluie@inuits.eu>
2012-07-06 14:30:59 +0200

    correct DNS and stop iptables

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,14 +2,20 @@ class site {
   file_line {
     'poor man\'s dns entry for dupond':
       ensure => present,
-      line   => '192.168.142.32 dupond',
+      line   => '192.168.142.32 dupond dupond.demo',
       path   => '/etc/hosts',
   }
   file_line {
     'poor man\'s dns entry for dupont':
       ensure => present,
-      line   => '191.168.142.31 dupont',
+      line   => '192.168.142.31 dupont dupont.demo',
       path   => '/etc/hosts',
+  }
+  service {
+    'iptables':
+      ensure    => stopped,
+      hasstatus => true,
+      enable    => false,
   }
 
 }
