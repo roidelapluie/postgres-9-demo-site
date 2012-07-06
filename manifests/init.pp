d2a0b625034e064752ebfae0d22f33163f9f5468
Julien Pivotto <roidelapluie@inuits.eu>
2012-07-06 12:52:09 +0200

    add poor man's DNS

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,15 @@
 class site {
+  file_line {
+    'poor man\'s dns entry for dupond':
+      ensure => present,
+      line   => '192.168.142.32 dupond',
+      path   => '/etc/hosts',
+  }
+  file_line {
+    'poor man\'s dns entry for dupont':
+      ensure => present,
+      line   => '191.168.142.31 dupont',
+      path   => '/etc/hosts',
+  }
 
 }
